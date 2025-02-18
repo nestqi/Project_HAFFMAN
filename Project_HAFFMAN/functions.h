96c37e71928ab700aefa7812df8dc8c620939808
misha-darko <2005.krawtpol@gmail.com>
2025-02-18 14:07:27 +0300

    добавил структуры

--- Project_HAFFMAN/functions.h
+++ Project_HAFFMAN/functions.h
@@ -1,18 +1,27 @@
-#pragma once
-class MyClass
-{
-public:
-	MyClass();
-	~MyClass();
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <algorithm>
 
-private:
+using namespace std;
 
+// Структура узла дерева Хаффмана
+struct HuffmanNode {
+    unsigned char data; // Символ (для листа)
+    int frequency;       // Частота символа
+    HuffmanNode* left;    // Левый дочерний узел
+    HuffmanNode* right;   // Правый дочерний узел
+
+    HuffmanNode(unsigned char data, int frequency) : data(data), frequency(frequency), left(nullptr), right(nullptr) {}
+    HuffmanNode(int frequency) : data(0), frequency(frequency), left(nullptr), right(nullptr) {}
 };
 
-MyClass::MyClass()
-{
-}
 
-MyClass::~MyClass()
-{
-}
+// Пользовательская функция сравнения для приоритетной очереди
+struct CompareNodes {
+    bool operator()(HuffmanNode* a, HuffmanNode* b) {
+        return a->frequency > b->frequency;
+    }
+};
