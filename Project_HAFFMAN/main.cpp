b635d71df0dc5044a019d2b33383387507da948e
misha-darko <2005.krawtpol@gmail.com>
2025-02-19 20:21:50 +0300

    тест война и мир(20с примерно)

--- Project_HAFFMAN/main.cpp
+++ Project_HAFFMAN/main.cpp
@@ -6,6 +6,7 @@ int main()
 {
     setlocale(LC_ALL, "RU");
 
+    /*
     auto start = std::chrono::high_resolution_clock::now();
     compressFile("input.txt", "compressed.huff");
     auto end = std::chrono::high_resolution_clock::now();
@@ -29,6 +30,19 @@ int main()
     end = std::chrono::high_resolution_clock::now();
     auto decompressBmpTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     std::cout << "‘айл compressed.huff_bmp распакован в decompressed.bmp за " << decompressBmpTime << " мс." << std::endl;
+    */
+
+    auto start = std::chrono::high_resolution_clock::now();
+    compressFile("warandpeace.txt", "compressed_warandpeace.huff");
+    auto end = std::chrono::high_resolution_clock::now();
+    auto compressTextTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    std::cout << "‘айл warandpeace.txt сжат в compressed_warandpeace.huff за " << compressTextTime << " мс." << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+    decompressFile("compressed_warandpeace.huff", "decompressed_warandpeace.txt");
+    end = std::chrono::high_resolution_clock::now();
+    auto decompressTextTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    std::cout << "‘айл compressed_warandpeace.huff распакован в decompressed_warandpeace.txt за " << decompressTextTime << " мс." << std::endl;
 
     return 0;
 }
